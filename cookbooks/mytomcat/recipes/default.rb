b9d61885a1b12b0ee91aab3e6a96ebb96f380ee8
Mahadev Patil <mahadev.patil@linuxmail.org>
2019-05-13 14:57:09 +0000

    added custom resource

---
+++ cookbooks/mytomcat/recipes/default.rb
@@ -0,0 +1,17 @@
+#
+# Cookbook:: mytomcat
+# Recipe:: default
+#
+# Copyright:: 2019, The Authors, All Rights Reserved.
+
+mytomcat 'tomcat' do
+  tar_url 'http://mirrors.estointernet.in/apache/tomcat/tomcat-9/v9.0.19/bin/apache-tomcat-9.0.19.tar.gz'
+  installation_dir '/opt'
+  action :install
+end
+
+mytomcat 'tomcat' do
+  tar_url 'http://mirrors.estointernet.in/apache/tomcat/tomcat-9/v9.0.19/bin/apache-tomcat-9.0.19.tar.gz'
+  action :restart
+end
+
---
+++ cookbooks/mytomcat/resources/tomcat.rb
@@ -0,0 +1,124 @@
+resource_name :mytomcat
+
+property :tomcat_name, String, name_property: true
+property :tar_url, String
+property :installation_dir, String, default: '/opt'
+
+action :install do
+
+  tomcat_tar = ::File.basename(URI.parse(new_resource.tar_url).path)
+  tomcat_dir = tomcat_tar[/apache-tomcat-\d+\.\d+\.\d+/]
+
+  package 'java-1.7.0-openjdk' do
+    action :remove
+  end
+
+  package 'java-1.8.0-openjdk' do
+    action :install
+  end
+
+  directory new_resource.installation_dir do
+    owner 'root'
+    group 'root'
+    mode '0755'
+    recursive true
+    action :create
+  end 
+
+  remote_file "#{new_resource.installation_dir}/#{tomcat_tar}" do
+    source new_resource.tar_url
+    owner 'root'
+    group 'root'
+    mode '0755'
+    action :create
+  end 
+
+  bash 'untar tomcat' do
+    cwd new_resource.installation_dir
+    code <<-EOH
+      tar -xf #{tomcat_tar}
+    EOH
+    action :run
+    not_if { ::File.exist?("#{new_resource.installation_dir}/#{tomcat_dir}") }
+  end
+
+end
+
+action :delete do
+
+  require 'fileutils'
+
+  tomcat_tar = ::File.basename(URI.parse(new_resource.tar_url).path)
+  tomcat_dir = tomcat_tar[/apache-tomcat-\d+\.\d+\.\d+/]
+
+  bash 'stop tomcat service' do
+    cwd "#{new_resource.installation_dir}/#{tomcat_dir}/bin"
+    code <<-EOF
+      sh shutdown.sh
+    EOF
+    action :run
+    only_if 'netstat -nlp | grep 8080'
+  end
+
+  FileUtils.rm_rf("#{new_resource.installation_dir}/#{tomcat_dir}")
+  FileUtils.rm_rf("#{new_resource.installation_dir}/#{tomcat_tar}")
+
+end
+
+action :start do
+
+  tomcat_tar = ::File.basename(URI.parse(new_resource.tar_url).path)
+  tomcat_dir = tomcat_tar[/apache-tomcat-\d+\.\d+\.\d+/]
+
+  bash 'start tomcat service' do
+    cwd "#{new_resource.installation_dir}/#{tomcat_dir}/bin"
+    code <<-EOF
+      sh startup.sh
+    EOF
+    action :run
+    not_if 'netstat -nlp | grep 8080'
+  end
+  sleep(20)
+end
+
+action :stop do
+
+  tomcat_tar = ::File.basename(URI.parse(new_resource.tar_url).path)
+  tomcat_dir = tomcat_tar[/apache-tomcat-\d+\.\d+\.\d+/]
+
+  bash 'stop tomcat service' do
+    cwd "#{new_resource.installation_dir}/#{tomcat_dir}/bin"
+    code <<-EOF
+      sh shutdown.sh
+    EOF
+    action :run
+    only_if 'netstat -nlp | grep 8080'
+  end
+  sleep(20)
+end
+
+action :restart do
+
+  tomcat_tar = ::File.basename(URI.parse(new_resource.tar_url).path)
+  tomcat_dir = tomcat_tar[/apache-tomcat-\d+\.\d+\.\d+/]
+
+  bash 'stop tomcat service' do
+    cwd "#{new_resource.installation_dir}/#{tomcat_dir}/bin"
+    code <<-EOF
+      sh shutdown.sh
+    EOF
+    action :run
+    only_if 'netstat -nlp | grep 8080'
+  end
+
+  sleep(20)
+  bash 'start tomcat service' do
+    cwd "#{new_resource.installation_dir}/#{tomcat_dir}/bin"
+    code <<-EOF
+      sh startup.sh
+    EOF
+    action :run
+    not_if 'netstat -nlp | grep 8080'
+  end
+
+end
